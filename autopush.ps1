c69cb90c9b989eb645e95355f39cfcd9a6e5eaa1
RyZenoKelb <ryzenotheking@gmail.com>
2025-05-27 18:40:06 +0200

    Auto update 2025-05-27 18:40:06

--- autopush.ps1
+++ autopush.ps1
@@ -1,11 +1,17 @@
+Set-Location -Path "$PSScriptRoot"
+
 while ($true) {
     git add -A
-    if (git diff --cached --quiet) {
+
+    git diff --cached --quiet
+    if ($LASTEXITCODE -eq 0) {
         Start-Sleep -Seconds 5
         continue
     }
+
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     git commit -m "Auto update $timestamp"
     git push origin main
+
     Start-Sleep -Seconds 5
 }
